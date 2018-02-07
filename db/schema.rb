5674118156b93554e163ba21980d81325c99f91b
stephesaenz <stephesaenz@gmail.com>
2018-02-07 10:24:17 -0600

    tweets work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207154911) do
+ActiveRecord::Schema.define(version: 20180207160600) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
